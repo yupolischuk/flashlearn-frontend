df55e4c6d0871556f4afe239c34eac98bb038ac6
yupolischuk <yumuscletank@gmail.com>
2018-08-31 13:16:55 +0300

    Started building UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          qwerty
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div className="navbar">
+                <button className="home-btn">Home</button>
+                <br/><br/>
+                <button className="add-deck-btn">Add new deck</button>
+                <button className="add-flashcard-btn">Add new flashcard</button>
+                <hr />
+            </div>
+        );
+    }
+}
+
+
+class Decks extends React.Component {
+
+    render() {
+        const decks = getDecks();
+        var listDecks = decks.map((deck) =>
+            <li>{deck['name']}
+                <button>Watch</button>
+                <button>Learn</button>
+                <button>Add Card</button>
+                <button>Update</button>
+                <button>Delete</button>
+            </li>
+        );
+
+        return (
+            <div className="decks-list">
+                Decks
+                {listDecks}
+            </div>
+        );
+    }
+}
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+                <Decks />
+            </div>
+        );
+    }
+}
+
+
+// ===================================================
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+function getDecks() {
+    return JSON.parse('[\n' +
+        '  {\n' +
+        '    "created": "Tue, 21 Aug 2018 15:49:40 GMT", \n' +
+        '    "id": 1, \n' +
+        '    "name": "Python", \n' +
+        '    "parent": null, \n' +
+        '    "updated": null\n' +
+        '  }, \n' +
+        '  {\n' +
+        '    "created": "Tue, 21 Aug 2018 15:49:46 GMT", \n' +
+        '    "id": 2, \n' +
+        '    "name": "Linux", \n' +
+        '    "parent": null, \n' +
+        '    "updated": null\n' +
+        '  }, \n' +
+        '  {\n' +
+        '    "created": "Tue, 21 Aug 2018 15:49:53 GMT", \n' +
+        '    "id": 3, \n' +
+        '    "name": "Data Structures", \n' +
+        '    "parent": null, \n' +
+        '    "updated": "Tue, 21 Aug 2018 15:50:03 GMT"\n' +
+        '  }\n' +
+        ']');
+}
+
+
